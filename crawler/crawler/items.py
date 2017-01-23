d2fea2727ea07fdbe2f33b45363228ca641b2186
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2017-01-23 15:34:27 +0100

    Add structure to the Ad item

--- crawler/crawler/items.py
+++ crawler/crawler/items.py
@@ -8,14 +8,32 @@
 import scrapy
 
 class Ad(scrapy.Item):
-    ad_id = scrapy.Field()
-    ref_no = scrapy.Field()
+    """Ad for all real estate advertisement
+    rent and buy
+    """
+    object_id = scrapy.Field()         # The internal id of the company
+    reference_no = scrapy.Field()      # The offical ref number
+    raw_data = scrapy.Field()          # The whole html site
+    crawler = scrapy.Field()           # which company was crawled
+    url = scrapy.Field()               # The url of the site
     street = scrapy.Field()
     place = scrapy.Field()
-    price_total = scrapy.Field()
+    price_brutto = scrapy.Field()
     price_netto = scrapy.Field()
     additional_costs = scrapy.Field()
-    configuration = scrapy.Field()
     description = scrapy.Field()
-    info = scrapy.Field()
-    last_updated = scrapy.Field(serializer=str)
+    living_area = scrapy.Field()
+    floor = scrapy.Field()             # on which floor
+    num_rooms = scrapy.Field()
+    num_floors = scrapy.Field()
+    build_year = scrapy.Field(serializer=str)
+    last_renovation_year = scrapy.Field(serializer=str)
+    cubature = scrapy.Field()
+    room_height = scrapy.Field()
+    effective_area = scrapy.Field()    # Nutzfläche
+    plot_area = scrapy.Field()         # Grundstückfläche
+    floors_house = scrapy.Field()      # how many floors the whole building have
+    characteristics = scrapy.Field()   # Additional information Seesicht, Lift/ Balkon/Sitzplatz
+    additional_data = scrapy.Field()   # Data at the moment we do not know where to put
+    owner = scrapy.Field()             # The name of the copany which insert the ad (if exists)
+
---
+++ crawler/crawler/utils.py
@@ -0,0 +1,43 @@
+# Dict for different languages
+# German / France / Italian
+
+FIELDS = {
+    'Etage': 'etage',
+    'Piano': 'etage',
+
+    'Objekttyp': 'objecttype',
+    'Type': 'objecttype',
+    'Tipo': 'objecttype',
+
+    'Zimmer': 'num_rooms',
+    'Pièces': 'num_rooms',
+    'Locali': 'num_rooms',
+
+    'Wohnfläche': 'living_area',
+    'Surface habitable': 'living_area',
+    'Superficie abitabile': 'living_area',
+
+    'Baujahr': 'build_year',
+    'Année de construction': 'build_year',
+    'Anno di costruzione': 'build_year',
+
+    'Nutzfläche': 'effective_area',
+    'Surface utile': 'effective_area',
+    'Superficie utile': 'effective_area',
+
+    'Kubatur': 'cubature',
+    'Volume': 'cubature',
+    'Cubatura': 'cubature',
+
+    'Raumhöhe': 'room_height',
+    'Hauteur des pièces': 'room_height',
+    'Altezza locali': 'room_height',
+
+    'Grundstückfläche': 'plot_area',
+    'Surface du terrain': 'plot_area',
+    'Superficie terreno': 'plot_area',
+
+    'Letzte Renovation': 'last_renovation_year',
+    'Dernière rénovation': 'last_renovation_year',
+    'Ultimo rinnovo': 'last_renovation_year'
+}
