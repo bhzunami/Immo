1bbdd3eaca398793be811ec54dbaeb22199359d1
Piero Steinger <piero@steinger.eu>
2017-01-24 14:04:49 +0100

    start middleware duplicate check

--- crawler/crawler/settings.py
+++ crawler/crawler/settings.py
@@ -52,9 +52,9 @@ COOKIES_ENABLED = False
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'crawler.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'crawler.middlewares.DuplicateCheck.DuplicateCheck': 100,
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
