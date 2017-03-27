7d4f00ee04fe551fb7a27d47d001b5eb7e2a722e
Piero Steinger <piero@steinger.eu>
2017-03-27 12:04:22 +0200

    fix pylint warnings

--- immo/models/models/utils.py
+++ immo/models/models/utils.py
@@ -9,52 +9,52 @@ Base = declarative_base()
 
 CANTONS = ["ZH", "BE", "LU", "UR", "SZ", "OW", "NW", "GL", "ZG", "FR", "SO", "BS", "BL", "SH", "AR", "AI", "SG", "GR", "AG", "TG", "TI", "VD", "VS", "NE", "GE", "JU"]
 
-def prepare_string(input):
-    if input is None:
+def prepare_string(s):
+    if s is None:
         return None
-    return input.replace("'", "").replace(",", ".")
+    return s.replace("'", "").replace(",", ".")
 
 ignore_values = ['Preis auf Anfrage', 'auf Anfrage' 'Erdgeschoss', 'EG', 'Parterre']
 
-def extract_number(input):
+def extract_number(num):
     try:
-        return re.search('[0-9]*\.?[0-9]+', input).group(0)
+        return re.search(r'[0-9]*\.?[0-9]+', num).group(0)
     except IndexError:
         return None
     except AttributeError:
-        if input == "UG":
+        if num == "UG":
             return -1
-        if input in ignore_values:
+        if num in ignore_values:
             return 0
 
-        print("ERROR REGEX INPUT: {}".format(input))
+        print("ERROR REGEX INPUT: {}".format(num))
         return None
 
 
-def get_int(input):
+def get_int(num):
     try:
-        return int(extract_number(prepare_string(input)))
+        return int(extract_number(prepare_string(num)))
     except TypeError:
         return None
 
-def get_float(input):
+def get_float(num):
     try:
-        return float(extract_number(prepare_string(input)))
+        return float(extract_number(prepare_string(num)))
     except TypeError:
         return None
 
-def get_date(input):
-    if input == "sofort":
+def get_date(s):
+    if s == "sofort":
         return date.today()
     try:
-        return datetime.strptime(input, '%d.%m.%Y')
+        return datetime.strptime(s, '%d.%m.%Y')
     except ValueError:
         pass
 
     return None
 
 def convert_to_int(num):
-    if type(num) == int:
+    if isinstance(num, int):
         return num
     num = num.replace(",", ".").replace(".–", "").replace("'", "")
     try:
