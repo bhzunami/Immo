1acbf9bdddb760ac350a5eee1b18a1d4ca17d461
Nicolas Mauchle <nicolas@nicolasmauchle.ch>
2017-08-07 09:25:10 +0200

    Add k-fold

--- immo/scikit/predict_pipeline.py
+++ immo/scikit/predict_pipeline.py
@@ -11,11 +11,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 # Scikit
 from sklearn.ensemble import ExtraTreesRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.externals import joblib
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.ensemble import AdaBoostRegressor
-
+import pdb
 from .helper import generate_matrix, plot, train_statistics
 
 RNG = np.random.RandomState(42)
@@ -32,10 +32,10 @@ class PredictPipeline(Pipeline):
         super().__init__(goal, settings, directory)
 
         predict_pipeline = [
-            self.outlier_detection,
-            self.adaBoost,
-            self.xgboost,
-            self.extraTreeRegression,
+            #self.outlier_detection,
+            #self.adaBoost,
+            #self.xgboost,
+            #self.extraTreeRegression,
             self.mix,
         ]
 
@@ -77,54 +77,64 @@ class PredictPipeline(Pipeline):
         model.fit(X_train, y_train)
         joblib.dump(model, '{}/extra_tree_regressior.pkl'.format(self.model_folder))
         y_pred = model.predict(X_test)
-        md = mape(y_test, y_pred)
         train_statistics(y_test, y_pred, title="ExtraTree_train")
         plot(y_test, y_pred, self.image_folder, show=True, title="ExtraTree_train")
         return ads
         
     def mix(self, ads):
         X, y = generate_matrix(ads, 'price')
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
-        adaboost = joblib.load('{}/adaboost.pkl'.format(self.model_folder))
-        a_predict = adaboost.predict(X_test)
-
-        xgb_model = xgb.XGBRegressor(silent=False, max_depth=100,
-                                     learning_rate=0.1, n_estimators=350, n_jobs=-1)
-        booster = xgb.Booster()
-        booster.load_model('{}/xgbooster.pkl'.format(self.model_folder))
-        xgb_model._Booster = booster
-        xg_predict = xgb_model.predict(X_test)
-
-        tree = joblib.load('{}/extra_tree_regressior.pkl'.format(self.model_folder))
-        t_predict = tree.predict(X_test)
+        X, y = X.values, y.values
 
 
-        y_pred = np.array(0.3*a_predict + 0.3*xg_predict + 0.4*t_predict)
-        train_statistics(y_test, y_pred, title="0.3, 0.3, 0.4")
 
-        y_pred = np.array(0.8*a_predict + 0.1*xg_predict + 0.1*t_predict)
-        train_statistics(y_test, y_pred, title="0.8, 0.1, 0.1")
+        for train_index, test_index in KFold(n_splits=3, shuffle=True).split(X):
+            logging.info('MIX new split')
+            logging.info("="*20)
 
-        y_pred = np.array(0.2*a_predict + 0.6*xg_predict + 0.2*t_predict)
-        train_statistics(y_test, y_pred, title="0.2, 0.6, 0.2")
+            adaboost = joblib.load('{}/adaboost.pkl'.format(self.model_folder))
+            xgb_model = xgb.XGBRegressor(silent=False, max_depth=100,
+                                        learning_rate=0.1, n_estimators=350, n_jobs=-1)
+            booster = xgb.Booster()
+            booster.load_model('{}/xgbooster.pkl'.format(self.model_folder))
+            xgb_model._Booster = booster
 
-        y_pred = np.array(0.4*a_predict + 0.4*xg_predict + 0.2*t_predict)
-        train_statistics(y_test, y_pred, title="0.4, 0.4, 0.2")
+            tree = joblib.load('{}/extra_tree_regressior.pkl'.format(self.model_folder))
 
-        y_pred = np.array(0.1*a_predict + 0.8*xg_predict + 0.1*t_predict)
-        train_statistics(y_test, y_pred, title="0.1, 0.8, 0.1")
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+       
+            a_predict = adaboost.predict(X_test)
+            xg_predict = xgb_model.predict(X_test)
+            t_predict = tree.predict(X_test)
+            y_pred = np.array(0.2*a_predict + 0.6*xg_predict + 0.2*t_predict)
+            train_statistics(y_test, y_pred, title="BEST")
 
-        y_pred = np.array(0.6*a_predict + 0.1*xg_predict + 0.3*t_predict)
-        train_statistics(y_test, y_pred, title="0.6, 0.1, 0.3")
+            y_pred = np.array(0.3*a_predict + 0.3*xg_predict + 0.4*t_predict)
+            train_statistics(y_test, y_pred, title="0.3, 0.3, 0.4")
 
-        y_pred = np.array(0.2*a_predict + 0.7*xg_predict + 0.1*t_predict)
-        train_statistics(y_test, y_pred, title="0.2, 0.7, 0.1")
+            y_pred = np.array(0.8*a_predict + 0.1*xg_predict + 0.1*t_predict)
+            train_statistics(y_test, y_pred, title="0.8, 0.1, 0.1")
 
-        y_pred = np.array(0.2*a_predict + 0.0*xg_predict + 0.8*t_predict)
-        train_statistics(y_test, y_pred, title="0.2, 0.0, 0.8")
+            y_pred = np.array(0.2*a_predict + 0.6*xg_predict + 0.2*t_predict)
+            train_statistics(y_test, y_pred, title="0.2, 0.6, 0.2")
 
-        y_pred = np.array(0.3*a_predict + 0.4*xg_predict + 0.3*t_predict)
-        train_statistics(y_test, y_pred, title="0.3, 0.4, 0.3")
+            y_pred = np.array(0.4*a_predict + 0.4*xg_predict + 0.2*t_predict)
+            train_statistics(y_test, y_pred, title="0.4, 0.4, 0.2")
+
+            y_pred = np.array(0.1*a_predict + 0.8*xg_predict + 0.1*t_predict)
+            train_statistics(y_test, y_pred, title="0.1, 0.8, 0.1")
+
+            y_pred = np.array(0.6*a_predict + 0.1*xg_predict + 0.3*t_predict)
+            train_statistics(y_test, y_pred, title="0.6, 0.1, 0.3")
+
+            y_pred = np.array(0.2*a_predict + 0.7*xg_predict + 0.1*t_predict)
+            train_statistics(y_test, y_pred, title="0.2, 0.7, 0.1")
+
+            y_pred = np.array(0.2*a_predict + 0.0*xg_predict + 0.8*t_predict)
+            train_statistics(y_test, y_pred, title="0.2, 0.0, 0.8")
+
+            y_pred = np.array(0.3*a_predict + 0.4*xg_predict + 0.3*t_predict)
+            train_statistics(y_test, y_pred, title="0.3, 0.4, 0.3")
 
         return ads
 
